8a21b86fda36b23b282b3d37ba9174b2ae25a470
algaboya <159569200+algaboya@users.noreply.github.com>
2024-05-12 20:37:28 +0400

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -31,30 +31,3 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 	}
 	return (NULL);
 }
-
-// char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
-// {
-// 	size_t	i;
-// 	size_t	j;
-
-// 	i = 0;
-// 	j = 0;
-// 	if (haystack == 00 && len == 00)
-// 		return (00);
-// 	if (ft_strlen(needle) == 0 || haystack == needle)
-// 		return ((char *)haystack);
-// 	if (needle[j] == '\0')
-// 	{
-// 		return ((char *)haystack);
-// 	}
-// 	while (haystack[i] != '\0' && i < len)
-// 	{
-// 		while (haystack[i + j] == needle[j] && needle[j] != '\0' && i + j < len)
-// 			j++;
-// 		if (needle[j] == '\0')
-// 			return ((char *) haystack + i);
-// 		i++;
-// 		j = 0;
-// 	}
-// 	return (00);
-// }
